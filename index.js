09240e3f7cf6ef52f33769b9191b183a6e1e706a
Flatiron School <github email address>
2016-12-07 12:01:45 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code solution here
+
+
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.homeState = homeState;
+    this.yearsInOffice = yearsInOffice
+  }
+
+
+  veto() {
+    return "NO!"
+  };
+
+
+  doCharity() {
+    return "I like to help people."
+  };
+
+  passBill() {
+    return "You can do that!"
+  }
+
+
+  conductPressInterview() {
+    return "I am proud to be an American."
+  }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`
+  }
+};
