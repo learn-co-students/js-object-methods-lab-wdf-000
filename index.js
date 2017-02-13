f761e34a3d0fd1daaac0aebb685bb83662d46e2d
Ekaterina Petushova <epetushova@gmail.com>
2017-02-13 18:58:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// code solution here
 class President{
 	constructor(name, politicalParty, yearsInOffice, homeState){
 		this.name = name;
@@ -6,24 +5,68 @@ class President{
 		this.yearsInOffice = yearsInOffice;
 		this.homeState = homeState;
 	}
-
 	veto(){
-	 	return("NO!");
+		return "NO!";
 	}
 
 	passBill(){
-		return ("You can do that!");
+		return "You can do that!";
 	}
 
 	doCharity(){
-		return ("I like to help people.");
+		return "I like to help people.";
 	}
+
 	conductPressInterview(){
-		return("I am proud to be an American.");
+		return "I am proud to be an American.";
 	}
 
 	sayHi(){
-		return("Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".")
+		return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// code solution here
+// class President{
+// 	constructor(name, politicalParty, yearsInOffice, homeState){
+// 		this.name = name;
+// 		this.politicalParty = politicalParty;
+// 		this.yearsInOffice = yearsInOffice;
+// 		this.homeState = homeState;
+// 	}
+
+// 	veto(){
+// 	 	return("NO!");
+// 	}
+
+// 	passBill(){
+// 		return ("You can do that!");
+// 	}
+
+// 	doCharity(){
+// 		return ("I like to help people.");
+// 	}
+// 	conductPressInterview(){
+// 		return("I am proud to be an American.");
+// 	}
+
+// 	sayHi(){
+// 		return("Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".")
+// 	}
+// }
 
